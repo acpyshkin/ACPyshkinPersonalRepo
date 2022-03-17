0acb5c9a2b5efacd644a01e2960a8c9da4a260f9
KabinA <a.c.pyshkin@hotmail.com>
2022-03-17 15:00:45 +0300

    Commit

---
+++ projects/DelegatesLambdasAndEvents/ConsoleDemoApp.Test/ArraySortTest.cs
@@ -0,0 +1,71 @@
+﻿namespace ConsoleDemoApp.Test
+{
+    using ConsoleDemoApp;
+    using NUnit.Framework;
+
+    [TestFixture]
+
+    internal class ArraySortTest
+    {
+        [TestCaseSource(nameof(DataForSortRows))]
+        public void SortRows_JaggedArrayIsCorrect_ArraySorted(int c, int[][] jaggedArrayToSort, int[][] expectedJaggedArray)
+        {
+            // arrange , act
+            switch (c)
+            {
+                case 1:
+                    ArraySort.SortRows(ArraySort.Sum, ArraySort.Desc, jaggedArrayToSort);
+                    break;
+                case 2:
+                    ArraySort.SortRows(ArraySort.MinValue, ArraySort.Asc, jaggedArrayToSort);
+                    break;
+                case 3:
+                    ArraySort.SortRows(ArraySort.MaxValue, ArraySort.Asc, jaggedArrayToSort);
+                    break;
+            }
+
+            // assert
+            Assert.AreEqual(jaggedArrayToSort, expectedJaggedArray);
+        }
+
+        private static readonly int[][] JaggedArrayToSort1 =
+        {
+            new int[] { 15, 0, 100, -791, 6 },
+            new int[] { -54985, 910, 392, 1940, -17 },
+            new int[] { 0, 9, 384, -809, 9160 },
+            new int[] { 8, 0, 57, 72, -45 },
+            new int[] { 669, 0, 0, 0, 22 }
+        };
+        private static readonly int[][] ExpectedJaggedArraySumDesc =
+        {
+            new int[] { -54985, 910, 392, 1940, -17 },
+            new int[] { 15, 0, 100, -791, 6 },
+            new int[] { 8, 0, 57, 72, -45 },
+            new int[] { 669, 0, 0, 0, 22 },
+            new int[] { 0, 9, 384, -809, 9160 }
+        };
+        private static readonly int[][] ExpectedJaggedArrayMinAsc =
+        {
+            new int[] { 669, 0, 0, 0, 22 },
+            new int[] { 8, 0, 57, 72, -45 },
+            new int[] { 15, 0, 100, -791, 6 },
+            new int[] { 0, 9, 384, -809, 9160 },
+            new int[] { -54985, 910, 392, 1940, -17 }
+        };
+        private static readonly int[][] ExpectedJaggedArrayMaxAsc =
+        {
+            new int[] { 0, 9, 384, -809, 9160 },
+            new int[] { -54985, 910, 392, 1940, -17 },
+            new int[] { 669, 0, 0, 0, 22 },
+            new int[] { 15, 0, 100, -791, 6 },
+            new int[] { 8, 0, 57, 72, -45 }
+        };
+
+        private static readonly List<TestCaseData> DataForSortRows = new List<TestCaseData>
+        {
+            new TestCaseData(1, JaggedArrayToSort1, ExpectedJaggedArraySumDesc),
+            new TestCaseData(2, JaggedArrayToSort1, ExpectedJaggedArrayMinAsc),
+            new TestCaseData(3, JaggedArrayToSort1, ExpectedJaggedArrayMaxAsc)
+        };
+    }
+}
---
+++ projects/DelegatesLambdasAndEvents/ConsoleDemoApp/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace ConsoleDemoApp
+{
+    using System;
+    using System.Text;
+    public class Program
+    {
+        public static void Main()
+        {
+            int[][] Matrix = new int[5][];
+            Matrix[0] = new int[] { 15, 0, 100, -791, 6 };
+            Matrix[1] = new int[] { -54985, 910, 392, 1940, -17 };
+            Matrix[2] = new int[] { 0, 9, 384, -809, 9160 };
+            Matrix[3] = new int[] { 8, 0, 57, 72, -45 };
+            Matrix[4] = new int[] { 669, 0, 0, 0, 22 };
+            ShowArray(Matrix);
+
+            ArraySort.SortRows(ArraySort.Sum, ArraySort.Desc, Matrix);
+            ShowArray(Matrix);
+
+            ArraySort.SortRows(ArraySort.MaxValue, ArraySort.Asc, Matrix);
+            ShowArray(Matrix);
+
+            ArraySort.SortRows(ArraySort.MinValue, ArraySort.Asc, Matrix);
+            ShowArray(Matrix);
+
+            Countdown countdown = new Countdown();
+            countdown.Subscrubing += Countdown_Subscrubing;
+            countdown.Subscrubing += Countdown_Subscrubing2;
+
+            countdown.Publish(3);
+            Thread.Sleep(5000);
+            countdown.Subscrubing += Countdown_Subscrubing3;
+            countdown.Subscrubing += Countdown_Subscrubing4;
+        }
+
+        private static void Countdown_Subscrubing(object? sender, SubscribingEventArgs e)
+        {
+            if (e.Message != null)
+            {
+                Console.WriteLine("Method 1" + e.Message);
+            }
+        }
+
+        private static void Countdown_Subscrubing2(object? sender, SubscribingEventArgs e)
+        {
+            if (e.Message != null)
+            {
+                Console.WriteLine("Method 2" + e.Message);
+            }
+        }
+
+        private static void Countdown_Subscrubing3(object? sender, SubscribingEventArgs e)
+        {
+            if (e.Message != null)
+            {
+                Console.WriteLine("Method 3" + e.Message);
+            }
+        }
+
+        private static void Countdown_Subscrubing4(object? sender, SubscribingEventArgs e)
+        {
+            if (e.Message != null)
+            {
+                Console.WriteLine("Method 4" + e.Message);
+            }
+        }
+
+        public static void ShowArray(int[][] arrayToShow)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int row = 0; row < arrayToShow.Length; row++)
+            {
+                for (int column = 0; column < arrayToShow[row].Length; column++)
+                {
+                    sb.Append(arrayToShow[row][column] + " ");
+                }
+
+                sb.Append('\n');
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
